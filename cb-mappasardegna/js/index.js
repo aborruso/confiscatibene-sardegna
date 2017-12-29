9ba2a5e5c183359d3db680f6343d391a2d15ca57
nelsonmau <andrea.nelson.mauro@gmail.com>
2017-12-29 21:59:27 +0100

    test 2 mappa...

--- cb-mappasardegna/js/index.js
+++ cb-mappasardegna/js/index.js
@@ -5,7 +5,7 @@ var map = L.mapbox.map('map', 'mapbox.streets')
     .setView([39.3, 9], 8);
 
 // carico i dati e li aggiungo sulla mappa
-// dati da https://docs.google.com/spreadsheets/d/1YdY0TASPL_hLWt2P9-LsNMK59IKMatWOOX1G7V1NWgw/edit#gid=0
+// dati da https://docs.google.com/spreadsheets/d/1xnVi1UjRAd50idDmXtKcYv-UxNWAE-rHL8qJeF3ef5o/edit#gid=0
 var gsheetSource = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRj_8QPjxAq9VDaTvU1xbR9ZSJls4pLY9jN0itafpMuqXgMT05oGNHeGG8bab1cTZF7_G_dL57AvB49/pub?gid=0&single=true&output=csv';
 var skyData = omnivore.csv(gsheetSource, null, L.mapbox.featureLayer()).addTo(map);
 
@@ -24,7 +24,7 @@ skyData.on('click', function(e) {
 
   // Modal Content
   $("#marker_title").html(title);
-  $("#marker_place").html("<i class='fa fa-map-marker' aria-hidden='true'></i> " + place + " - <i class='fa fa-calendar' aria-hidden='true'></i> "+ date);
+  $("#marker_place").html("<i class='fa fa-map-marker' aria-hidden='true'></i> " + place + " - <i class='fa fa-calendar' aria-hidden='true'></i> ");
   $("#marker_content").html(sommario + "<br><br>" + content);
 //  $("#marker_contacts").html("<i class='fa fa-address-card' aria-hidden='true'></i> " + email + " | " + website);
   $('#exampleModal').modal('show');
