649975b000957eb9184e62ee05d630a18a20bf58
nelsonmau <andrea.nelson.mauro@gmail.com>
2018-01-11 14:53:46 +0100

    aggiunto dossier embed

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
+var ugS llify = require('gulp-uglify');
 var pkg = require('./package.json');
 
 // Set the banner content
